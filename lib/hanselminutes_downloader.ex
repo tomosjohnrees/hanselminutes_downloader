708231cb188f401d61c5963341835d5ff845535d
Tomos John Rees <sabt@live.co.uk>
2016-01-04 18:08:59 +0000

    Add function to find all urls on a page via a url

--- lib/hanselminutes_downloader.ex
+++ lib/hanselminutes_downloader.ex
@@ -1,9 +1,16 @@
 defmodule HanselminutesDownloader do
-  def podcast_urls do
+  def find_podcast_urls do
     %HTTPoison.Response{body: body} = HTTPoison.get!("http://hanselminutes.com/archives")
     body
     |> Floki.find(".showCard")
     |> Floki.attribute("href")
     |> Enum.map(fn(path) -> "http://hanselminutes.com" <> path end)
+  end
+
+  def find_all_urls(url) do
+    %HTTPoison.Response{body: body} = HTTPoison.get!(url)
+    body
+    |> Floki.find("a")
+    |> Floki.attribute("href")
   end
 end
