be57ab9222843feba9cead054c18acf2070f7684
Tomos John Rees <sabt@live.co.uk>
2016-01-04 17:57:26 +0000

    Add function to get podcast urls

--- lib/hanselminutes_downloader.ex
+++ lib/hanselminutes_downloader.ex
@@ -1,2 +1,9 @@
 defmodule HanselminutesDownloader do
+  def podcast_urls do
+    %HTTPoison.Response{body: body} = HTTPoison.get!("http://hanselminutes.com/archives")
+    body
+    |> Floki.find(".showCard")
+    |> Floki.attribute("href")
+    |> Enum.map(fn(path) -> "http://hanselminutes.com" <> path end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule HanselminutesDownloader.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule HanselminutesDownloader.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.8.0"},
+      {:floki, "~> 0.7"}
+    ]
   end
 end
