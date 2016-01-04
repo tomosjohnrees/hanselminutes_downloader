db0ff920ee5e614d3c06c51459ca49d27bf0334d
Tomos John Rees <sabt@live.co.uk>
2016-01-04 22:26:08 +0000

    Add function to check if a url is an mp3 url. Add function to filter a list of url's for mp3's.

--- lib/hanselminutes_downloader.ex
+++ lib/hanselminutes_downloader.ex
@@ -14,6 +14,24 @@ defmodule HanselminutesDownloader do
     |> Floki.attribute("href")
   end
 
+# e.g. filter_for_mp3 [".com/bla/file.mp3", "www.bla.com/about.index"]
+#      => [".com/bla/file.mp3"]
+  def filter_for_mp3(list) do
+    list
+    |> Enum.filter(fn(url) -> url |> is_mp3? end)
+  end
+
+  def is_mp3?([ "3", "p", "m", "." | _ ]),
+    do: true
+  def is_mp3?([ _ | _ ]),
+    do: false
+  def is_mp3?(url) do
+    url
+    |> String.codepoints
+    |> Enum.reverse
+    |> is_mp3?
+  end
+
   def extract_mp3_name(url) do
     url
     |> String.codepoints
